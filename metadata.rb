d2a4f9448e60675a54709e46827db9a07abe9fcb
Blake Irvin <b.irvin@modcloth.com>
2012-08-02 15:01:57 -0700

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "ModCloth, Inc."
+maintainer_email "b.irvin@modcloth.com"
+license          "Apache 2.0"
+description      "Installs/Configures nad"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
