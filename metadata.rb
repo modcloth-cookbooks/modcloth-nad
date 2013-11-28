8036007b43769c363b24b30f1ed50e9ce7b8de2c
Rafe Colton <r.colton@modcloth.com>
2013-11-27 16:27:18 -0800

    Bumping version so old checks don't break

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'github+nad-cookbook@modcloth.com'
 license          'MIT'
 description      'Installs/Configures nad'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 
 %w(
   centos
